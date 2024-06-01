cdf63f77b3e62436352d61b73c5bfc9c21fc9316
alex-1900 <omytty@126.com>
2024-06-01 21:56:14 +0800

    udpate

--- bbs/star_rail/sync_role.py
+++ bbs/star_rail/sync_role.py
@@ -1,8 +1,12 @@
+import html
 import json
 import os
+import re
+from urllib.parse import unquote
 
 import pdfkit
 import requests
+from bs4 import BeautifulSoup
 
 from bbs._functions import handle_response
 
@@ -12,8 +16,7 @@ class SyncRole:
     同步角色信息
     """
 
-    _pdfkit_config = pdfkit.configuration(wkhtmltopdf='D:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe')
-    _pdf_html_buffer = ''
+    # _pdfkit_config = pdfkit.configuration(wkhtmltopdf='D:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe')
 
     _headers = {
         'Accept': 'application/json, text/plain, */*',
@@ -59,18 +62,94 @@ class SyncRole:
         res = self._get_role_struct(_content_id)
 
         _html = f'<section><header>以下为角色[{res['role_name']}]的信息：</header>'
+        _ext = ''
         for _content in res['contents']:
             _html = f'{_html}<h1>{_content['name']}</h1>{_content['text']}'
+            _e = self._get_compute_points(_content['text'])
+            if _e:
+                _ext = f'{_ext}<p>{_e}</p>'
 
-        self._pdf_html_buffer = f'{self._pdf_html_buffer}{_html}<footer>以上为角色[{res['role_name']}]的所有信息</footer></section>'
+        self._pdf_html_buffer = f'{self._pdf_html_buffer}{_html}<p>角色行迹和角色属性信息：<br />{_ext}</p><footer>以上为角色[{res['role_name']}]的所有信息</footer></section>'
 
-    def create_roles_pdf(self):
+    def _get_compute_points(self, _text):
+        _pattern = r'行迹面板中的行迹图标.+?data-data="([^"]+?)"'
+        _match = re.search(_pattern, _text, re.M)
+        _res = ''
+        if _match:
+            _points = _match.group(1)
+            _html = unquote(_points)
+            _json = BeautifulSoup(_html, 'html.parser').get_text()
+            _data_dict = json.loads(_json)
+            for _item_dict in _data_dict:
+                if _item_dict['partKey'] == 'trace':
+                    _role_id = _item_dict['data']['attr']['roleId']
+                    _skill_html = self._get_skill_html(_item_dict['data']['attr']['points'], _role_id)
+                    # print(_skill_html)
+                    _res = f'{_res}<p>{_skill_html}</p>'
+                if _item_dict['partKey'] == 'gainMethod':
+                    _method_html = self._get_method_html(_item_dict['data']['gainMethod'])
+                    _res = f'{_res}<p>{_method_html}</p>'
+        return _res
+
+    def _fetch_skill_list(self, _role_id, _point_id):
+        url = 'https://api-static.mihoyo.com/common/blackboard/sr_wiki/v1/compute_point'
+        res = self._session.get(url, params={
+            'app_sn': 'sr_wiki',
+            'point_id': _point_id,
+            'role_id': _role_id
+        })
+
+        _resp = handle_response(res)
+        _list = _resp['list']
+        _html = f'共{len(_list)}级，'
+        for _item in _list:
+            _html = f'{_html}{_item['level']}级所需材料：'
+            if len(_item['list']) == 0:
+                _html = f'{_html}无'
+            else:
+                for _good in _item['list']:
+                    _html = f'{_html}{_good['num']}{_good['item_name']}、'
+            _html = f'{_html}；'
+        return _html
+
+    def _get_method_html(self, _json):
+        _html = '<h1>角色属性：</h1><table><thead><tr><th>属性名称</th><th>详情</th></tr></thead><tbody>'
+        for _method in _json:
+            _html = f"""{_html}
+                    <tr>
+                    <td>{_method['key']}</td>
+                    <td>{_method['value']}</td>
+                    </tr>
+                    """
+        return f'{_html}</tbody></table>'
+
+    def _get_skill_html(self, _json, _role_id):
+        _html = '<h1>角色行迹：</h1><table><thead><tr><th>行迹类型</th><th>行迹名称</th><th>详情说明</th><th>升级材料</th></tr></thead><tbody>'
+        for _key, skill in enumerate(_json):
+            _skill_ext_html = self._fetch_skill_list(_role_id, _key + 1)
+            _type_name = skill['name'].split()
+            _html = f"""{_html}
+            <tr>
+            <td>{_type_name[0]}</td>
+            <td>{_type_name[1]}</td>
+            <td>{skill['desc']}</td>
+            <td>{_skill_ext_html}</td>
+            </tr>
+            """
+        return f'{_html}</tbody></table>'
+
+
+    def create_roles_pdf(self, number):
         self._pdf_html_buffer = f'{self._pdf_html_buffer}</body></html>'
 
-        _pdf_path = f'dist/star_role/roles/崩坏星穹铁道角色.pdf'
+        _pdf_path = f'dist/star_role/roles/崩坏星穹铁道角色{number}.pdf'
         _directory = os.path.dirname(_pdf_path)
 
         os.makedirs(_directory) if not os.path.exists(_directory) else None
         os.remove(_pdf_path) if os.path.exists(_pdf_path) else None
 
-        pdfkit.from_string(self._pdf_html_buffer, _pdf_path, configuration=self._pdfkit_config)
+        try:
+            # , configuration=self._pdfkit_config
+            pdfkit.from_string(self._pdf_html_buffer, _pdf_path)
+        except Exception as e:
+            pass
--- commands/star_rail/sync_roles.py
+++ commands/star_rail/sync_roles.py
@@ -1,12 +1,44 @@
-import os
-import pdfkit
-
 from bbs import BBSSyncRole
 
 
 def sync_roles():
     bbs_sync_role = BBSSyncRole()
+    bbs_sync_role.update_roles_data(1543)
+    bbs_sync_role.update_roles_data(1325)
     bbs_sync_role.update_roles_data(1187)
-    bbs_sync_role.update_roles_data(2526)
+    bbs_sync_role.update_roles_data(497)
+    bbs_sync_role.create_roles_pdf(1)
+    print('完成1')
 
-    bbs_sync_role.create_roles_pdf()
+
+    bbs_sync_role = BBSSyncRole()
+    bbs_sync_role.update_roles_data(412)
+    bbs_sync_role.update_roles_data(561)
+    bbs_sync_role.update_roles_data(76)
+    bbs_sync_role.update_roles_data(46)
+    bbs_sync_role.create_roles_pdf(2)
+    print('完成2')
+
+    bbs_sync_role = BBSSyncRole()
+    bbs_sync_role.update_roles_data(423)
+    bbs_sync_role.update_roles_data(422)
+    bbs_sync_role.update_roles_data(386)
+    bbs_sync_role.update_roles_data(380)
+    bbs_sync_role.create_roles_pdf(3)
+    print('完成3')
+
+    bbs_sync_role = BBSSyncRole()
+    bbs_sync_role.update_roles_data(49)
+    bbs_sync_role.update_roles_data(1717)
+    bbs_sync_role.update_roles_data(52)
+    bbs_sync_role.update_roles_data(406)
+    bbs_sync_role.create_roles_pdf(4)
+    print('完成4')
+
+    bbs_sync_role = BBSSyncRole()
+    bbs_sync_role.update_roles_data(48)
+    bbs_sync_role.update_roles_data(1710)
+    bbs_sync_role.update_roles_data(565)
+    bbs_sync_role.update_roles_data(564)  # 白露
+    bbs_sync_role.create_roles_pdf(5)
+    print('完成5')
